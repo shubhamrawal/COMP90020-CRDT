d94b03465f690853eee0699ca2cb87ca7a2d21db
sangclee17 <sangclee17@gmail.com>
2018-05-05 20:49:56 +1000

    modified to ArrayList holdbackQueue and check implemented checkHoldbackQueue

--- src/main/java/messenger/ordering/MessageBuffer.java
+++ src/main/java/messenger/ordering/MessageBuffer.java
@@ -1,21 +1,43 @@
 package messenger.ordering;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class MessageBuffer {
-	Queue<TimestampedMessage> queue;
 	
-	public MessageBuffer(){
-		queue = new LinkedList<TimestampedMessage>();
-	}
+	private ArrayList<TimestampedMessage> holdBackQueue;
+	private VectorTimestamp vectorTs;
 	
-	public TimestampedMessage dequeue(TimestampedMessage msg) {
-		return queue.remove();
+	public MessageBuffer(VectorTimestamp vectorTs) {
+		holdBackQueue = new ArrayList<TimestampedMessage>();
+		this.vectorTs = vectorTs;
 	}
 	
 	public void enqueue(TimestampedMessage msg) {
-		queue.add(msg);
+		holdBackQueue.add(msg);
+	}
+	
+	public void dequeue(TimestampedMessage msg) {
+		if (holdBackQueue.contains(msg)) {
+			holdBackQueue.remove(msg);
+		}
+	}
+	
+	public TimestampedMessage checkHoldbackQueue() {
+		VectorTimestamp other;
+		UUID sender;
+		int msgIndex;
+		if (!holdBackQueue.isEmpty()) {
+			for (int i = 0; i < holdBackQueue.size(); i++) {
+				other = holdBackQueue.get(i).getTimestamp();
+				sender = holdBackQueue.get(i).getProcessId();
+				if (vectorTs.isDeliverable(other, sender)){
+					msgIndex = holdBackQueue.indexOf(holdBackQueue.get(i));
+					return holdBackQueue.remove(msgIndex);
+				}
+			}
+		}
+		return null;
 	}
 
 }
