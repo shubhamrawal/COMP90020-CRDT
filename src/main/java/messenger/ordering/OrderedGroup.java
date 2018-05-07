2e188f2a06684fe16b42929e259e1107de0d7dfb
sangclee <sangclee17@gmail.com>
2018-05-08 06:07:12 +1000

    filter out past messages

--- src/main/java/messenger/ordering/OrderedGroup.java
+++ src/main/java/messenger/ordering/OrderedGroup.java
@@ -24,43 +24,26 @@ public class OrderedGroup<M extends Message> implements Group<M> {
         this.timestamp.increment(App.uuid);
         messageGroup.send(new OrderedMessage<M>(App.uuid, this.timestamp, message));
     }
-
+    
     public void onReceipt(Callback<M> callback) {
-        this.messageGroup.onReceipt(message -> receive(message, callback));
-    }
-
-    void receive(OrderedMessage<M> message, Callback<M> callback) {
-        // first, compare local timestamp with timestamp of received message
-        int order = timestamp.compareTo(message.getTimestamp(), message.getSenderId());
-        if (order == 0) {
-            // if received timestamp is expected deliver the message
-            timestamp.merge(message.getTimestamp());
-            if (callback != null) {
-                callback.process(message.getInnerMessage());
-            } else {
-                LOGGER.info("No callback attached -> message ignored");
-            }
-        } else if (order == -1) {
-            // if received timestamp lies in the future, buffer message
-            messageBuffer.add(message);
-        }
-        // otherwise ignore message as it has already been delivered in the past
-
-        // check the message buffer for deliverable messages and deliver them if possible
-        messageBuffer.applyAndRemoveIfTrue(
-                bufferedMessage -> this.timestamp.compareTo(bufferedMessage.getTimestamp(),
-                        bufferedMessage.getSenderId())
-                        == 0,
-                bufferedMessage -> {
-                    timestamp.merge(bufferedMessage.getTimestamp());
-                    if (callback != null) {
-                        callback.process(bufferedMessage.getInnerMessage());
-                    } else {
-                        LOGGER.info("No callback attached -> message ignored");
-                    }
-                    return true;
-                });
-    }
+		this.messageGroup.onReceipt(message -> {
+			// filter the message generated in the past in case it got re-delivered
+			if (!timestamp.isPast(message.getTimestamp())){	
+				if(timestamp.isDeliverable(message.getTimestamp(), message.getSenderId())) {
+					timestamp.merge(message.getTimestamp());
+					callback.process(message.getInnerMessage());
+				}
+				else {
+					messageBuffer.add(message);
+				}
+				if (messageBuffer.checkbuffer()!= null) {
+					timestamp.merge(messageBuffer.checkbuffer().getTimestamp());
+					callback.process(messageBuffer.checkbuffer().getInnerMessage());
+				}
+			}
+		}
+		);
+	}
 
     public void join() {
         messageGroup.join();
