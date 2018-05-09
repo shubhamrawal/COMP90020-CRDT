882c99d16c9330f4d4b10d0f2c6f677c6f4a0f4d
sangclee <sangclee17@gmail.com>
2018-05-10 01:34:22 +1000

    splits testing cases

--- src/test/java/messenger/ordering/VectorTimestampTest.java
+++ src/test/java/messenger/ordering/VectorTimestampTest.java
@@ -7,27 +7,25 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public class VectorTimestampTest {
-
+	//vectorTimestamp of process1
+	UUID uuid1 = UUID.fromString("235569AB-F4F5-47D7-92C1-9B14505CFFA1");
+    VectorTimestamp vTimestamp1 = new VectorTimestamp();
+    
+    //vectorTimestamp of process2
+    UUID uuid2 = UUID.fromString("9CEBC96F-51BE-4A7B-8376-F75BFCD0F24E");
+    VectorTimestamp vTimestamp2 = new VectorTimestamp();
+    
+    //vectorTimestamp of process3
+    UUID uuid3 = UUID.fromString("62F26D29-7A74-467E-9CA4-0D8A20F176EA");
+    VectorTimestamp vTimestamp3 = new VectorTimestamp();
+	
     @Test
-    public void test() {
-    	//vectorTimestamp of process1
-    	UUID uuid1 = UUID.fromString("235569AB-F4F5-47D7-92C1-9B14505CFFA1");
-    	HashMap<UUID,Integer> logicalClock1 = new HashMap<UUID,Integer>();
-    	logicalClock1.put(uuid1,0);
-        VectorTimestamp vTimestamp1 = new VectorTimestamp(logicalClock1);
-        
-        //vectorTimestamp of process2 and an event happened at process 2
-        UUID uuid2 = UUID.fromString("9CEBC96F-51BE-4A7B-8376-F75BFCD0F24E");
-        VectorTimestamp vTimestamp2 = new VectorTimestamp();
-        vTimestamp2.increment(uuid2);
-        
-        //vectorTimestamp of process3
-        UUID uuid3 = UUID.fromString("62F26D29-7A74-467E-9CA4-0D8A20F176EA");
-        HashMap<UUID,Integer> logicalClock3 = new HashMap<UUID,Integer>();
-        logicalClock3.put(uuid3, 0);
-        VectorTimestamp vTimestamp3 = new VectorTimestamp();
-        
+    public void testNormalCase() {
+    	
         //1. normal multicast case
+    	//event happened at process 2
+    	vTimestamp2.increment(uuid2);
+    	
         //expected to be deliverable
         Assert.assertEquals(0, vTimestamp1.compareTo(vTimestamp2, uuid2));
         Assert.assertEquals(0, vTimestamp3.compareTo(vTimestamp2, uuid2));
@@ -36,11 +34,23 @@ public class VectorTimestampTest {
         vTimestamp1.merge(vTimestamp2);
         vTimestamp3.merge(vTimestamp2);
         
-        // all processes are expected to have same vector clock
+        // all processes are expected to have same vector clock as (0,1,0)
         Assert.assertEquals(vTimestamp1, vTimestamp3);
         Assert.assertEquals(vTimestamp3, vTimestamp2);
         
-        // 2. buffered case!!
+        
+        
+        //3. past case!!
+        //when process 3 receives the past message from both process 1 and 2
+        // expected past message to process 3
+        vTimestamp3.increment(uuid3);
+        Assert.assertEquals(1, vTimestamp3.compareTo(vTimestamp1, uuid1));
+        Assert.assertEquals(1, vTimestamp3.compareTo(vTimestamp2, uuid2));
+    }
+    
+    // proceed with the same vector clock (0,1,0) for all three processes
+    @Test
+    public void testBufferedCase() {
         //event happened at process 1
         vTimestamp1.increment(uuid1);
         
@@ -49,29 +59,35 @@ public class VectorTimestampTest {
         //merge process2 to (1,1,0)
         vTimestamp2.merge(vTimestamp1);
         
-        //and event happened again at process 2 and send it to process 3
-        //before process 3 received the message from process1
+        //and event happened again at process 2 
         vTimestamp2.increment(uuid2);
-        
-        //expected to be future message to process 3 then
+        //process 3 received the message sent by process 2 first
+        //before it received the message sent by process1
+        //expected to be future message at process 3
         Assert.assertEquals(-1, vTimestamp3.compareTo(vTimestamp2, uuid2));
         
-        //wait for message from process 1 before merge with process 3
+        //wait for the message from process 1 before merge with process 3
         vTimestamp3.merge(vTimestamp1);
+        // expected to be deliverable after being merged with message sent by process 1
+        Assert.assertEquals(0, vTimestamp3.compareTo(vTimestamp2, uuid2));
         vTimestamp3.merge(vTimestamp2);
         
-        //expected to be deliverable for process 1 for the message from process 2
+        //expected to be deliverable at process 1 for the message sent by process 2
         Assert.assertEquals(0, vTimestamp1.compareTo(vTimestamp2, uuid2));
         vTimestamp1.merge(vTimestamp2);
         
-        //expected to have all the same vectorclock as (1,2,0)
+        //expected to have all the same vector clock as (1,2,0)
         Assert.assertEquals(vTimestamp1, vTimestamp2);
         Assert.assertEquals(vTimestamp2, vTimestamp3);
-        
-        //3. past case!!
-        //when process 3 receives the past message from both process 1 and 2
-        // expected past message to process 3
-        vTimestamp3.increment(uuid3);
+    }
+    
+    // proceed with the same vector clock as (1,2,0) for all three processes
+    @Test
+    public void testPastCase() {
+    	//event happened at process 3
+    	vTimestamp3.increment(uuid3);
+    	//process 3 was received past message sent by process1 and 2
+        // expected to be past message to process 3
         Assert.assertEquals(1, vTimestamp3.compareTo(vTimestamp1, uuid1));
         Assert.assertEquals(1, vTimestamp3.compareTo(vTimestamp2, uuid2));
     }
