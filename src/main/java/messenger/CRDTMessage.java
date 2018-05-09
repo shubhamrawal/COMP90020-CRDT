b6c5e031de86dcb664aee949530cd32d27f74b8e
Ferdinand Swoboda <ferdinand.swoboda@googlemail.com>
2018-05-09 15:55:42 +1000

    Remove redundant sender id in crdt message
    Signed-off-by: Ferdinand Swoboda <ferdinand.swoboda@googlemail.com>

--- src/main/java/messenger/CRDTMessage.java
+++ src/main/java/messenger/CRDTMessage.java
@@ -4,33 +4,17 @@ import crdt.Operation;
 import messenger.message.Message;
 
 import java.util.Objects;
-import java.util.UUID;
 
 public class CRDTMessage implements Message {
 
-    private UUID senderId;
     private Operation operation;
 
-    public CRDTMessage(UUID senderId, Operation operation) {
-
-        this.senderId = senderId;
+    public CRDTMessage(Operation operation) {
         this.operation = operation;
-    }
-
-    public UUID getSenderId() {
-        return senderId;
-    }
-
-    public void setSenderId(UUID senderId) {
-        this.senderId = senderId;
     }
 
     public Operation getOperation() {
         return operation;
-    }
-
-    public void setOperation(Operation operation) {
-        this.operation = operation;
     }
 
     @Override
@@ -38,8 +22,7 @@ public class CRDTMessage implements Message {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         CRDTMessage that = (CRDTMessage) o;
-        return Objects.equals(getSenderId(), that.getSenderId()) &&
-                Objects.equals(getOperation(), that.getOperation());
+        return Objects.equals(getOperation(), that.getOperation());
     }
 
 }
