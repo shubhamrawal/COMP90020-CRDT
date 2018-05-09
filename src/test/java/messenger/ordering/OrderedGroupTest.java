103b1ce3fb6042b838e49695622494b02b04f1a9
Ferdinand Swoboda <ferdinand.swoboda@googlemail.com>
2018-05-09 20:40:43 +1000

    Add tests for OrderedGroup
    Signed-off-by: Ferdinand Swoboda <ferdinand.swoboda@googlemail.com>

--- src/test/java/messenger/ordering/OrderedGroupTest.java
+++ src/test/java/messenger/ordering/OrderedGroupTest.java
@@ -12,9 +12,9 @@ import texteditor.App;
 import java.util.HashMap;
 import java.util.UUID;
 
-import static org.junit.Assert.fail;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static texteditor.App.uuid;
 
 public class OrderedGroupTest {
 
@@ -32,31 +32,123 @@ public class OrderedGroupTest {
         orderedGroup.leave();
 
         HashMap<UUID, Integer> expectedLClocks = new HashMap<>();
-        expectedLClocks.put(App.uuid, 1);
-        verify(groupMock).send(new OrderedMessage<>(App.uuid, new VectorTimestamp(expectedLClocks), message));
-    }
-
-    @Test
-    public void messagesAreDeliveredInOrder() {
-        //TODO add test
-        fail();
+        expectedLClocks.put(uuid, 1);
+        verify(groupMock).send(new OrderedMessage<>(uuid, new VectorTimestamp(expectedLClocks), message));
     }
 
     @Test
     public void pastMessageIsIgnored() {
-        //TODO add test
-        fail();
+        Group<OrderedMessage<Message>> groupMock = mock(Group.class);
+        OrderedGroup<Message> orderedGroup = new OrderedGroup<Message>(groupMock);
+        Message sentMessage = mock(Message.class);
+        Callback<Message> callback = mock(Callback.class);
+
+        //received stuff
+        Message innerMessage = mock(Message.class);
+        HashMap<UUID, Integer> logicalClocks = new HashMap<>();
+        logicalClocks.put(App.uuid, 1);
+        OrderedMessage receivedMessage = new OrderedMessage(App.uuid, new VectorTimestamp(logicalClocks), innerMessage);
+
+        // set up the scene
+        orderedGroup.onReceipt(callback);
+        orderedGroup.join();
+        orderedGroup.send(sentMessage);
+        orderedGroup.send(sentMessage);
+
+        //local timestamp is now (2, 0, 0, ...)
+        // received timestamp is (1, 0, 0, ...)
+        orderedGroup.receive(receivedMessage, callback);
+
+        verify(callback, never()).process(any());
     }
 
     @Test
     public void expectedMessageIsDeliveredImmediately() {
-        //TODO add test
-        fail();
+        Group<OrderedMessage<Message>> groupMock = mock(Group.class);
+        OrderedGroup<Message> orderedGroup = new OrderedGroup<Message>(groupMock);
+        Message sentMessage = mock(Message.class);
+        Callback<Message> callback = mock(Callback.class);
+
+        //received stuff
+        Message innerMessage = mock(Message.class);
+        HashMap<UUID, Integer> logicalClocks = new HashMap<>();
+        logicalClocks.put(App.uuid, 3);
+        OrderedMessage receivedMessage = new OrderedMessage(App.uuid, new VectorTimestamp(logicalClocks), innerMessage);
+
+        // set up the scene
+        orderedGroup.onReceipt(callback);
+        orderedGroup.join();
+        orderedGroup.send(sentMessage);
+        orderedGroup.send(sentMessage);
+
+        //local timestamp is now (2, 0, 0, ...)
+        // received timestamp is (3, 0, 0, ...)
+        orderedGroup.receive(receivedMessage, callback);
+
+        verify(callback).process(innerMessage);
     }
 
     @Test
-    public void futureMessageIsBuffered() {
-        //TODO add test
-        fail();
+    public void firstMessageFromUnknownProcessIsDeliveredImmediately() {
+        Group<OrderedMessage<Message>> groupMock = mock(Group.class);
+        OrderedGroup<Message> orderedGroup = new OrderedGroup<Message>(groupMock);
+        Message sentMessage = mock(Message.class);
+        Callback<Message> callback = mock(Callback.class);
+
+        //received stuff
+        Message innerMessage = mock(Message.class);
+        HashMap<UUID, Integer> logicalClocks = new HashMap<>();
+        logicalClocks.put(App.uuid, 1);
+        UUID senderId = UUID.randomUUID();
+        logicalClocks.put(senderId, 1);
+        OrderedMessage receivedMessage = new OrderedMessage(senderId, new VectorTimestamp(logicalClocks), innerMessage);
+
+        // set up the scene
+        orderedGroup.onReceipt(callback);
+        orderedGroup.join();
+        orderedGroup.send(sentMessage);
+        orderedGroup.send(sentMessage);
+
+        //local timestamp is now (2, 0, 0, ...)
+        // received timestamp is (1, 0, 1, ...)
+        orderedGroup.receive(receivedMessage, callback);
+
+        verify(callback).process(innerMessage);
+    }
+
+    @Test
+    public void futureMessageIsBufferedAndDeliveredInOrder() {
+        Group<OrderedMessage<Message>> groupMock = mock(Group.class);
+        OrderedGroup<Message> orderedGroup = new OrderedGroup<Message>(groupMock);
+        Message sentMessage = mock(Message.class);
+        Callback<Message> callback = mock(Callback.class);
+
+        // received stuff
+        Message innerMessage = mock(Message.class);
+        HashMap<UUID, Integer> logicalClocks = new HashMap<>();
+        logicalClocks.put(App.uuid, 4);
+        OrderedMessage receivedMessage = new OrderedMessage(App.uuid, new VectorTimestamp(logicalClocks), innerMessage);
+
+        // set up the scene
+        orderedGroup.onReceipt(callback);
+        orderedGroup.join();
+        orderedGroup.send(sentMessage);
+        orderedGroup.send(sentMessage);
+
+        // local timestamp is now (2, 0, 0, ...)
+        // received timestamp is (4, 0, 0, ...)
+        orderedGroup.receive(receivedMessage, callback);
+
+        verify(callback, never()).process(any());
+
+        HashMap<UUID, Integer> logicalClocks2 = new HashMap<>();
+        logicalClocks.put(App.uuid, 3);
+        OrderedMessage receivedMessage2 = new OrderedMessage(App.uuid, new VectorTimestamp(logicalClocks2), innerMessage);
+
+        //local timestamp is still (2, 0, 0, ...)
+        //received timestamp is (4, 0, 0, ...)
+        orderedGroup.receive(receivedMessage2, callback);
+
+        verify(callback, times(2)).process(innerMessage);
     }
 }
